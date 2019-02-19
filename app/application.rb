98a58e3b06303541b435cc32a31e5db3dc46ffbe
Maximo Bautista <max.bautista97@gmail.com>
2019-02-19 21:19:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,8 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-  
-
-    if DateTime.new.hour < 12
+    
+    if Time.now.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
